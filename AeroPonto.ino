d42723ba33b94c037473acff7547c2a089bc3762
Lucas Silva <86630007+Lucassilv7@users.noreply.github.com>
2024-04-24 14:14:02 -0300

    Add files via upload

--- AeroPonto.ino
+++ AeroPonto.ino
@@ -1,55 +1,70 @@
-//Programa: Teste modulo cartao micro SD Arduino
-//Autor: Arduino e Cia
-#include <SPI.h>
-#include <SD.h>
-//Pino de conexão do pino CS do modulo
-const byte chipSelect = 4;
-const byte buttonPin = 2;
-const byte ledPin = 5;
+#include <Wire.h> //INCLUSÃO DA BIBLIOTECA NECESSÁRIA
+#include <SPI.h> //INCLUSÃO DE BIBLIOTECA
+#include <MFRC522.h> //INCLUSÃO DE BIBLIOTECA
 
-byte buttonState = 0;
+#define SS_PIN 10 //PINO SDA
+#define RST_PIN 9 //PINO DE RESET
 
-void setup()
-{
-  pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+MFRC522 rfid(SS_PIN, RST_PIN); //PASSAGEM DE PARÂMETROS REFERENTE AOS PINOS
 
+struct AeroPoint{
+  String tag, name; 
+}
+
+AeroPoint aeroPoint[6]; 
+
+void setup(){
   Serial.begin(9600);
-  Serial.println("Inicializando cartao SD...");
+  Wire.begin(); //INICIALIZA A BIBLIOTECA WIRE
+  SPI.begin(); //INICIALIZA O BARRAMENTO SPI
+  rfid.PCD_Init(); //INICIALIZA MFRC522
   
-  // Inicia comunicacao com o modulo SD
-  if (!SD.begin(chipSelect))
-  {
-    Serial.println("Falha ao acessar o cartao !");
-    return;
-  }
-  
-  Serial.println("Cartao iniciado corretamente !");
-  Serial.println();
+
+  aeroPoint[0].name = "Victória";
+  aeroPoint[0].tag = "F3:05:9D:AA";
+  aeroPoint[1].name = "Sanzia";
+  aeroPoint[1].tag = "C3:BC:1A:AD";
+  aeroPoint[2].name = "Max Sena";
+  aeroPoint[2].tag = "63:6D:16:AD";
+  aeroPoint[3].name = "José";
+  aeroPoint[3].tag = "A3:40:30:AD";
+  aeroPoint[4].name = "Erick";
+  aeroPoint[4].tag = "43:9F:95:AC";
+  aeroPoint[5].name = "Paulo";
+  aeroPoint[5].tag = "F3:89:AE:1B";
 }
 
-void loop()
-{
-  buttonState = digitalRead(buttonPin);
-  unsigned long currentMillis = millis();
- 
-  // Abre o arquivo datalog.txt
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
-  
-  // Grava as informacoes no arquivo
-  if (dataFile) {
-    if (buttonState == HIGH) {
-      dataFile.print("Batido ponto de Paulo");
-      digitalWrite(ledPin, HIGH);
+void loop() {
+  leituraRfid(); //CHAMA A FUNÇÃO RESPONSÁVEL PELA VALIDAÇÃO DA TAG RFID
+}
 
-      Serial.println("Batido ponto de Paulo");  // Log
-    }
+//FUNÇÃO DE VALIDAÇÃO DA TAG RFID
+void leituraRfid(){
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) //VERIFICA SE O CARTÃO PRESENTE NO LEITOR É DIFERENTE DO ÚLTIMO CARTÃO LIDO. CASO NÃO SEJA, FAZ
+    return; //RETORNA PARA LER NOVAMENTE
+
+  /***INICIO BLOCO DE CÓDIGO RESPONSÁVEL POR GERAR A TAG RFID LIDA***/
+  String strID = ""; 
+  for (byte i = 0; i < 4; i++) {
+    strID +=
+    (rfid.uid.uidByte[i] < 0x10 ? "0" : "") +
+    String(rfid.uid.uidByte[i], HEX) +
+    (i!=3 ? ":" : "");
+  }
+  strID.toUpperCase();
+/***FIM DO BLOCO DE CÓDIGO RESPONSÁVEL POR GERAR A TAG RFID LIDA***/
+
+  //O ENDEREÇO "27:41:AA:AB" DEVERÁ SER ALTERADO PARA O ENDEREÇO DA SUA TAG RFID QUE CAPTUROU ANTERIORMENTE
+  for(int i = 0; i < 7; i++){
+    if (strID.indexOf(aeroPoint[i].tag) >= 0) { //SE O ENDEREÇO DA TAG LIDA FOR IGUAL AO ENDEREÇO INFORMADO, FAZ
+      Serial.println(aeroPoint[i].name);
+      // Data, Horar e nome da pessoa
+      // Pensar em uma 
+    }else{ //SENÃO, FAZ (CASO A TAG LIDA NÃO SEJÁ VÁLIDA)
     
-    dataFile.close();
-  } else {
-    Serial.println("Erro ao abrir o arquivo datalog.txt");
+    }
   }
 
-  delay(2000);
-  digitalWrite(ledPin, LOW);
-}
+  rfid.PICC_HaltA(); //PARADA DA LEITURA DO CARTÃO
+  rfid.PCD_StopCrypto1(); //PARADA DA CRIPTOGRAFIA NO PCD
+  }
